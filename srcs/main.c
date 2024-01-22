7acd103a29d1015ce917d4a558fabe600fd07aec
Dries Van Den Brande <dvan-den@fu-r4-p1.s19.be>
2024-01-22 12:42:39 +0100

    Documentation

--- srcs/main.c
+++ srcs/main.c
@@ -14,6 +14,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * @brief Frees the memory allocated for a dynamically allocated
+ * string array.
+ *
+ * This function iterates through the elements of the string array and
+ * frees the memory for each element.
+ *
+ * @param str_array A pointer to the dynamically allocated string array.
+ */
 void	free_string_array(char **str_array)
 {
 	int	i;
@@ -29,6 +38,16 @@ void	free_string_array(char **str_array)
 	free(str_array);
 }
 
+/**
+ * @brief Helper function for main that handles the sorting based
+ * on the size of stack A.
+ *
+ * This function performs sorting based on the size of stack A. It
+ * either swaps or sorts the stacks accordingly.
+ *
+ * @param a A pointer to the head of stack A.
+ * @param b A pointer to the head of stack B.
+ */
 void	main_help(t_stack_node *a, t_stack_node *b)
 {
 	if (stack_len(a) == 2)
@@ -39,6 +58,16 @@ void	main_help(t_stack_node *a, t_stack_node *b)
 		sort_stacks(&a, &b);
 }
 
+/**
+ * @brief Main function for the sorting algorithm using stacks.
+ *
+ * This function initializes stacks A and B, processes the input,
+ * and performs the sorting algorithm based on the stack size.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of strings containing the command-line arguments.
+ * @return 0 on successful execution.
+ */
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
